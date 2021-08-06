a0169256560bd799718bdd8f539fc55cdecd9d3f
Viktor Zakharov <vtk.live@gmail.com>
2021-08-06 23:19:26 +0300

    first commit

---
+++ setup_scripts/setup_scripts/raspberry_i3wm.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+
+DWL_FOLDER=$HOME/git
+
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+BLUE=$'\033[0;34m'
+PURPLE=$'\033[0;35m'
+CYAN=$'\033[0;36m'
+WHITE=$'\033[0;37m'
+YELLOW=$'\033[0;33m'
+BOLD=$'\033[1m'
+OFF=$'\033[0m'
+
+sudo apt update
+sudo apt full-upgrade
+sudo apt install -y git stow
+
+mkdir -p $HOME/.config
+rm -f $HOME/.bashrc
+
+git clone https://github.com/Tapik600/dotfiles.git --depth 1 $HOME/dotfiles
+cd $HOME/dotfiles
+stow *
+
+source $HOME/.bashrc
+
+printf $CYAN$BOLD'\n\n== %s %.30s>\n\n'$OFF \
+"Installing windows manager" \
+"========================================================="
+sudo apt install -y \
+                 build-essential \ 
+                 python3-sphinx \
+                 python3-packaging \
+                 python3-xcbgen    
+                 xserver-xorg \
+                 xinit \
+                 x11-xserver-utils
+                 dh-autoreconf \
+                 xcb \
+                 libpango1.0-dev \
+                 libyajl-dev libev-dev \
+                 libjsoncpp-dev
+                 libstartup-notification0-dev \
+                 libxkbcommon-dev \
+                 libxkbcommon-x11-dev \
+                 libxcb1-dev \
+                 libxcb-keysyms1-dev \
+                 libxcb-icccm4-dev \
+                 libxcb-cursor-dev \
+                 libxcb-util0-dev \
+                 libxcb-randr0-dev \
+                 libxcb-xinerama0-dev \
+                 libxcb-xkb-dev \
+                 libxcb-xrm0 \
+                 libxcb-xrm-dev \
+                 libxcb-shape0 \
+                 libxcb-shape0-dev \
+                 libcairo2-dev \
+                 libxcb-ewmh-dev \
+                 libxcb-image0-dev \
+                 libxcb-composite0-dev \
+                 pkg-config \
+                 python-xcbgen \
+                 xcb-proto \
+                 meson \
+                 cmake \
+                 cmake-data
+
+printf $YELLOW'\n\n%s %.30s>\n\n'$OFF \
+"Installing i3-gaps" \
+".........................................................."
+git clone https://github.com/Airblader/i3.git --depth 1 $DWL_FOLDER/i3
+
+cd $DWL_FOLDER/i3 && mkdir build && cd build
+meson ..
+ninja
+sudo ninja install
+
+printf $YELLOW'\n\n%s %.30s>\n\n'$OFF \
+"Installing polybar" \
+".........................................................."
+git clone --recursive https://github.com/polybar/polybar $DWL_FOLDER/polybar
+
+cd $DWL_FOLDER/polybar && mkdir build && cd build
+cmake ..
+make -j$(nproc)
+# Optional. This will install the polybar executable in /usr/local/bin
+sudo make install
+
+printf $YELLOW'\n\n%s %.30s>\n\n'$OFF \
+"Download GTK+ GruvBox Theme " \
+".........................................................."
+THEMES_FOLDER=$HOME/.themes
+ICONS_FOLDER=$HOME/.icons
+
+mkdir $THEMES_FOLDER $ICONS_FOLDER
+
+if [ ! -d "$DWL_FOLDER/gruvbox-material-gtk" ]
+then
+  git clone https://github.com/sainnhe/gruvbox-material-gtk.git --depth 1 $DWL_FOLDER/gruvbox-material-gtk
+fi
+
+if [ ! -d "$THEMES_FOLDER/gruvbox-material-gtk" ]
+then
+  cp -r $DWL_FOLDER/gruvbox-material-gtk/themes/* $THEMES_FOLDER
+  cp -r $DWL_FOLDER/gruvbox-material-gtk/icons/* $ICONS_FOLDER
+fi
+
+printf $YELLOW'\n\n%s %.30s>\n\n'$OFF \
+"Installing software" \
+".........................................................."
+sudo apt install -y \
+                 rxvt-unicode \
+                 rofi \
+                 feh \
+                 htop \
+                 ranger \
+                 chromium-browser \
+                 rpi-chromium-mods \
+                 lxappearance \
+                
+printf $CYAN$BOLD'\n\n== %s %.30s>\n\n'$OFF \
+"Installing C++ compilers" \
+"========================================================="
+#!/bin/bash
+
+printf $YELLOW'\n\n%s %.30s>\n\n'$OFF \
+"Installing GCC-10.1.0" \
+".........................................................."
+git clone https://bitbucket.org/sol_prog/raspberry-pi-gcc-binary.git --depth 1 $DWL_FOLDER/gcc
+cd $DWL_FOLDER/gcc
+tar -xjvf gcc-10.1.0-armhf-raspbian.tar.bz2
+sudo mv gcc-10.1.0 /opt
+
+sudo ln -s /usr/include/arm-linux-gnueabihf/sys /usr/include/sys
+sudo ln -s /usr/include/arm-linux-gnueabihf/bits /usr/include/bits
+sudo ln -s /usr/include/arm-linux-gnueabihf/gnu /usr/include/gnu
+sudo ln -s /usr/include/arm-linux-gnueabihf/asm /usr/include/asm
+sudo ln -s /usr/lib/arm-linux-gnueabihf/crti.o /usr/lib/crti.o
+sudo ln -s /usr/lib/arm-linux-gnueabihf/crt1.o /usr/lib/crt1.o
+sudo ln -s /usr/lib/arm-linux-gnueabihf/crtn.o /usr/lib/crtn.o
+
+sudo cp /usr/include/arm-linux-gnueabihf/sys/* /usr/include/sys/
+
+# printf $YELLOW'\n\n%s %.30s>\n\n'$OFF \
+# "Installing CLANG-9.0.0" \
+# ".........................................................."
+# cd $DWL_FOLDER
+# wget http://releases.llvm.org/9.0.0/clang+llvm-9.0.0-armv7a-linux-gnueabihf.tar.xz
+# tar -xvf clang+llvm-9.0.0-armv7a-linux-gnueabihf.tar.xz
+# rm clang+llvm-9.0.0-armv7a-linux-gnueabihf.tar.xz
+# mv clang+llvm-9.0.0-armv7a-linux-gnueabihf clang_9.0.0
+# sudo mv clang_9.0.0 /usr/local
+
+git clone https://github.com/abhiTronix/TBB_Raspberry_pi.git --depth 1 $DWL_FOLDER/tbb
+cd $DWL_FOLDER/tbb
+sudo dpkg -i libtbb-dev_2018-U2_arm.deb 
+sudo ldconfig 
+
+# gcc-10.1 --version
+# clang++ --version
+
+printf $CYAN$BOLD'\n\n== %s %.30s>\n\n'$OFF \
+"Installing VSCode" \
+"========================================================="
+sudo apt install -y code
+
+code --install-extension pkief.material-icon-theme 
+code --install-extension gracefulpotato.gruvbox-ish 
+code --install-extension ms-vscode.cpptools 
+code --install-extension cschlosser.doxdocgen
+code --install-extension jeff-hykin.better-cpp-syntax
+code --install-extension ms-vscode-remote.remote-ssh 
+
+code --enable-proposed-api pkief.material-icon-theme 
+# code --enable-proposed-api gracefulpotato.gruvbox-ish 
+
+
+cd $HOME/dotfiles
+stow -D setup_scripts
+cd $HOME
