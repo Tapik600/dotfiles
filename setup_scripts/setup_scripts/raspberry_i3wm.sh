17ed3ab6da70ae32305d4d7f35f0ecaa38df6a33
Viktor Zakharov <vtk.live@gmail.com>
2021-08-07 21:32:29 +0300

    fix rpi_i3 setup script

--- setup_scripts/setup_scripts/raspberry_i3wm.sh
+++ setup_scripts/setup_scripts/raspberry_i3wm.sh
@@ -12,8 +12,13 @@ YELLOW=$'\033[0;33m'
 BOLD=$'\033[1m'
 OFF=$'\033[0m'
 
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+
 sudo apt update
-sudo apt full-upgrade
+sudo apt full-upgrade -y
 sudo apt install -y git stow
 
 mkdir -p $HOME/.config
@@ -24,52 +29,57 @@ cd $HOME/dotfiles
 stow *
 
 source $HOME/.bashrc
+chmod +x $HOME/.config/polybar/launch.sh
+chmod +x $HOME/.config/polybar/updates.sh
 
-printf $CYAN$BOLD'\n\n== %s %.30s>\n\n'$OFF \
+printf $CYAN$BOLD'\n== %s %.30s>\n\n'$OFF \
 "Installing windows manager" \
 "========================================================="
-sudo apt install -y \
-                 build-essential \ 
-                 python3-sphinx \
-                 python3-packaging \
-                 python3-xcbgen    
-                 xserver-xorg \
-                 xinit \
-                 x11-xserver-utils
-                 dh-autoreconf \
-                 xcb \
-                 libpango1.0-dev \
-                 libyajl-dev libev-dev \
-                 libjsoncpp-dev
-                 libstartup-notification0-dev \
-                 libxkbcommon-dev \
-                 libxkbcommon-x11-dev \
-                 libxcb1-dev \
-                 libxcb-keysyms1-dev \
-                 libxcb-icccm4-dev \
-                 libxcb-cursor-dev \
-                 libxcb-util0-dev \
-                 libxcb-randr0-dev \
-                 libxcb-xinerama0-dev \
-                 libxcb-xkb-dev \
-                 libxcb-xrm0 \
-                 libxcb-xrm-dev \
-                 libxcb-shape0 \
-                 libxcb-shape0-dev \
-                 libcairo2-dev \
-                 libxcb-ewmh-dev \
-                 libxcb-image0-dev \
-                 libxcb-composite0-dev \
-                 pkg-config \
-                 python-xcbgen \
-                 xcb-proto \
-                 meson \
-                 cmake \
-                 cmake-data
+sudo apt install --no-install-recommends -y \
+                xserver-xorg \
+                xinit \
+                x11-xserver-utils 
 
-printf $YELLOW'\n\n%s %.30s>\n\n'$OFF \
+sudo apt install -y \
+                xcb \
+                xcb-proto \
+                libxcb1-dev \
+                libxcb-keysyms1-dev \
+                libxcb-icccm4-dev \
+                libxcb-cursor-dev \
+                libxcb-util0-dev \
+                libxcb-randr0-dev \
+                libxcb-xinerama0-dev \
+                libxcb-xkb-dev \
+                libxcb-xrm0 \
+                libxcb-xrm-dev \
+                libxcb-shape0 \
+                libxcb-shape0-dev \
+                libcairo2-dev \
+                libxcb-ewmh-dev \
+                libxcb-image0-dev \
+                libxcb-composite0-dev \
+                libxkbcommon-dev \
+                libxkbcommon-x11-dev \
+                libpango1.0-dev \
+                libyajl-dev libev-dev \
+                libjsoncpp-dev \
+                libstartup-notification0-dev \
+                build-essential \
+                python-xcbgen \
+                python3-sphinx \
+                python3-packaging \
+                dh-autoreconf \
+                libiw-dev \
+                pkg-config \
+                meson \
+                cmake \
+                cmake-data
+
+printf $YELLOW'\n%s %.30s\n\n'$OFF \
 "Installing i3-gaps" \
 ".........................................................."
+
 git clone https://github.com/Airblader/i3.git --depth 1 $DWL_FOLDER/i3
 
 cd $DWL_FOLDER/i3 && mkdir build && cd build
@@ -77,9 +87,10 @@ meson ..
 ninja
 sudo ninja install
 
-printf $YELLOW'\n\n%s %.30s>\n\n'$OFF \
+printf $YELLOW'\n%s %.30s\n\n'$OFF \
 "Installing polybar" \
 ".........................................................."
+
 git clone --recursive https://github.com/polybar/polybar $DWL_FOLDER/polybar
 
 cd $DWL_FOLDER/polybar && mkdir build && cd build
@@ -88,9 +99,10 @@ make -j$(nproc)
 # Optional. This will install the polybar executable in /usr/local/bin
 sudo make install
 
-printf $YELLOW'\n\n%s %.30s>\n\n'$OFF \
+printf $YELLOW'\n%s %.30s\n\n'$OFF \
 "Download GTK+ GruvBox Theme " \
 ".........................................................."
+
 THEMES_FOLDER=$HOME/.themes
 ICONS_FOLDER=$HOME/.icons
 
@@ -107,27 +119,14 @@ then
   cp -r $DWL_FOLDER/gruvbox-material-gtk/icons/* $ICONS_FOLDER
 fi
 
-printf $YELLOW'\n\n%s %.30s>\n\n'$OFF \
-"Installing software" \
-".........................................................."
-sudo apt install -y \
-                 rxvt-unicode \
-                 rofi \
-                 feh \
-                 htop \
-                 ranger \
-                 chromium-browser \
-                 rpi-chromium-mods \
-                 lxappearance \
-                
-printf $CYAN$BOLD'\n\n== %s %.30s>\n\n'$OFF \
+printf $CYAN$BOLD'\n== %s %.30s>\n\n'$OFF \
 "Installing C++ compilers" \
 "========================================================="
-#!/bin/bash
 
-printf $YELLOW'\n\n%s %.30s>\n\n'$OFF \
+printf $YELLOW'\n%s %.30s\n\n'$OFF \
 "Installing GCC-10.1.0" \
 ".........................................................."
+
 git clone https://bitbucket.org/sol_prog/raspberry-pi-gcc-binary.git --depth 1 $DWL_FOLDER/gcc
 cd $DWL_FOLDER/gcc
 tar -xjvf gcc-10.1.0-armhf-raspbian.tar.bz2
@@ -155,28 +154,45 @@ sudo cp /usr/include/arm-linux-gnueabihf/sys/* /usr/include/sys/
 
 git clone https://github.com/abhiTronix/TBB_Raspberry_pi.git --depth 1 $DWL_FOLDER/tbb
 cd $DWL_FOLDER/tbb
-sudo dpkg -i libtbb-dev_2018-U2_arm.deb 
-sudo ldconfig 
+sudo dpkg -i libtbb-dev_2018-U2_arm.deb
+sudo ldconfig
 
 # gcc-10.1 --version
 # clang++ --version
 
-printf $CYAN$BOLD'\n\n== %s %.30s>\n\n'$OFF \
+printf $CYAN$BOLD'\n== %s %.30s>\n\n'$OFF \
 "Installing VSCode" \
 "========================================================="
+
 sudo apt install -y code
 
-code --install-extension pkief.material-icon-theme 
-code --install-extension gracefulpotato.gruvbox-ish 
-code --install-extension ms-vscode.cpptools 
+code --install-extension pkief.material-icon-theme
+code --install-extension gracefulpotato.gruvbox-ish
 code --install-extension cschlosser.doxdocgen
 code --install-extension jeff-hykin.better-cpp-syntax
-code --install-extension ms-vscode-remote.remote-ssh 
+code --install-extension twxs.cmake
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode-remote.remote-ssh
 
-code --enable-proposed-api pkief.material-icon-theme 
-# code --enable-proposed-api gracefulpotato.gruvbox-ish 
-
+code --enable-proposed-api pkief.material-icon-theme
+# code --enable-proposed-api gracefulpotato.gruvbox-ish
 
 cd $HOME/dotfiles
 stow -D setup_scripts
 cd $HOME
+
+printf $YELLOW'\n%s %.30s\n\n'$OFF \
+"Installing software" \
+".........................................................."
+
+sudo apt install -y \
+                 rxvt-unicode \
+                 rofi \
+                 feh \
+                 htop \
+                 ranger \
+                 chromium-browser \
+                 rpi-chromium-mods \
+                 lxappearance \
+                 udisks2
